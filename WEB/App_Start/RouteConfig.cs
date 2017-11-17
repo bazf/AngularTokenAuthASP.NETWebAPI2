489f0a8a02e5d0ca15a3d61f927d0b6f871b3b91
fvasyl <vaquuf@gmail.com>
2017-11-17 22:56:17 +0200

    Added WEB Project

---
+++ WEB/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WEB
+{
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapPageRoute(
+            routeName: "AngularRoute",
+            routeUrl: "index",
+            physicalFile: "~/index.html");
+            //physicalFile: "~/clientSide/dist/index.html");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
